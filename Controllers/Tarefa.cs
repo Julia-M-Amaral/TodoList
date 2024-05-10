c0c12ed7049280d820bd449f48e50b2ca84f3e37
Julia "jubsz <JuliaAmaralSantos@outlook.com>
2024-05-10 04:45:43 -0300

    Add project files.

---
+++ Controllers/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList.Controllers
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string? Descrição { get; set; }
+        public DateOnly DataInicio { get; set; }
+        public DateOnly DataConclusao { get; set; }
+        public bool Concluido { get; set; }
+    }
+}
